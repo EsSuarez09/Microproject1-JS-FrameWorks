67fb042207c57f90b7819dd3d7e6a67e29a22eb1
MariaEs09 <200545408@student.georgianc.on.ca>
2024-08-08 10:46:27 -0400

    arrange comments

--- sports.js
+++ sports.js
@@ -1,17 +1,25 @@
-import express from 'express'; // Importing the Express module
-import path from 'path'; // Importing the Path module for handling file paths
-import data from './data/sports_data.json' assert { type: 'json' }; // Importing the JSON data
+// Importing the Express module
+import express from "express"; 
+// Importing the Path module for handling file paths
+import path from "path"; 
+// Importing the JSON data
+import data from "./data/sports_data.json" assert { type: "json" }; 
+// Creating an Express application
+const app = express(); 
+// Defining the port number for the server
+const port = 3000; 
 
-const app = express(); // Creating an Express application
-const port = 3000; // Defining the port number for the server
-
-app.use(express.static('public')); // Serve static files from the 'public' directory
-
-app.get('/api/sports', (req, res) => { // Define a route to handle GET requests to '/api/sports'
-  res.json(data); // Send the JSON data as a response
+// Serve static files from the 'public' directory
+app.use(express.static("public")); 
+// Define a route to handle GET requests to '/api/sports'
+app.get("/api/sports", (req, res) => {
+  
+  // Send the JSON data as a response
+  res.json(data); 
 });
 
-
-app.listen(port, () => { // Start the server and listen on the specified port
-  console.log(`Server is running at http://localhost:${port}`); // Log a message when the server starts
+// Start the server and listen on the specified port
+app.listen(port, () => {
+  // Log a message when the server starts
+  console.log(`Server is running at http://localhost:${port}`); 
 });
